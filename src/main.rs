33592a87f7dba797972caacb0cd1937b28e02224
TwoC-Mr <1191422391@qq.com>
2025-10-01 22:50:18 +0800

    完成-第三章基础代码

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 
 use parking_lot::Mutex;
 use std::sync::Arc;
+use wgpu::include_wgsl;
 use winit::{
     application::ApplicationHandler,
     dpi::PhysicalSize,
@@ -24,6 +25,8 @@ struct WgpuApp {
     size: winit::dpi::PhysicalSize<u32>,
     size_changed: bool,
     clear_color: wgpu::Color,
+    // 第3章渲染管线
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl WgpuApp {
@@ -70,6 +73,56 @@ impl WgpuApp {
         };
         surface.configure(&device, &config);
         let clear_color = wgpu::Color::BLACK;
+
+        let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                compilation_options: Default::default(),
+                entry_point: Some("vs_main"),
+                buffers: &[],
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                compilation_options: Default::default(),
+                entry_point: Some("fs_main"),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw, // 2.
+                cull_mode: Some(wgpu::Face::Back),
+                // 将此设置为 Fill 以外的任何值都要需要开启 Feature::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // 需要开启 Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // 需要开启 Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None, // 1.
+            multisample: wgpu::MultisampleState {
+                count: 1,                         // 2.
+                mask: !0,                         // 3.
+                alpha_to_coverage_enabled: false, // 4.
+            },
+            multiview: None, // 5.
+            cache: None,
+        });
+
         Self {
             window,
             surface,
@@ -81,6 +134,8 @@ impl WgpuApp {
             size_changed: false,
             _adapter: adapter,
             clear_color,
+            // 第三章-渲染管线
+            render_pipeline,
         }
     }
 
@@ -131,19 +186,27 @@ impl WgpuApp {
             });
 
         {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                     view: &view,
                     resolve_target: None,
                     depth_slice: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
                         store: wgpu::StoreOp::Store,
                     },
                 })],
                 ..Default::default()
             });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
         }
 
         self.queue.submit(Some(encoder.finish()));
